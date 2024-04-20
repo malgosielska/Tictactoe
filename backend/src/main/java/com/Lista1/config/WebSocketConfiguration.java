4fe98c0590e522b8c00e0f79bcfb86a22080e6a8
malgosielska <malgosielska@hmail.com>
2024-04-20 14:39:22 +0200

    Added elastic_beanstalk

--- backend/src/main/java/com/Lista1/config/WebSocketConfiguration.java
+++ backend/src/main/java/com/Lista1/config/WebSocketConfiguration.java
@@ -2,10 +2,7 @@ package com.Lista1.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -14,7 +11,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/gameplay")
-                .setAllowedOrigins("*")
+                .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 
